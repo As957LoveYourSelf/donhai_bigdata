f7cfeb9fcf86451d4a61d7ecbf087de334b7896f
As957LoveYourSelf <1290450579@qq.com>
2022-01-06 16:33:57 +0800

    一些修改

--- src/main/java/com/zcq/travelweb/Controller/IndexController.java
+++ src/main/java/com/zcq/travelweb/Controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
